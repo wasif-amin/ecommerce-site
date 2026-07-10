a0b4b5989e975ee587ba8580c941b0e17da21019
wasif-amin <aminwasif96@gmail.com>
2026-07-09 21:02:18 -0400

    define app

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:21070212w@localhost:5432/ecommercesite'
